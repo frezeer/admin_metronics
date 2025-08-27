f52550b0d9e3436307e0f08dcfb57352b2044543
Sergio Morales <frezee13@hotmail.com>
2025-08-27 14:34:25 -0600

    cambio de nombres a español

--- src/app/modules/configuration/sucursal-deliveries/create-surcursal-deliveries/create-surcursal-deliveries.component.ts
+++ src/app/modules/configuration/sucursal-deliveries/create-surcursal-deliveries/create-surcursal-deliveries.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { ToastrService } from 'ngx-toastr';
+import { SucursalDeliverieService } from '../service/sucursal-deliverie.service';
 
 @Component({
   selector: 'app-create-surcursal-deliveries',
@@ -6,5 +9,59 @@ import { Component } from '@angular/core';
   styleUrls: ['./create-surcursal-deliveries.component.scss']
 })
 export class CreateSurcursalDeliveriesComponent {
+
+    @Output() SucursalDeliverieC: EventEmitter<any> = new EventEmitter();
+    @Input() sucursal:any = [];
+
+       isLoading:      any;
+       name:           string = '';
+       address:        string = '';
+
+        constructor(
+          public modal: NgbActiveModal,
+          public sucursalDeliverieService: SucursalDeliverieService , // Assuming RolesService is injected here
+          public toast: ToastrService,
+        ) {
+
+        }
+
+        ngOnInit(): void {
+
+        }
+
+        store(){
+
+          if(!this.name){
+            this.toast.error('validacion', 'El nombre de Direccion de Entrega es obligatorio');
+            return false;
+          }
+
+             if(!this.address){
+            this.toast.error('validacion', 'La direccion de Direccion de Entrega es obligatorio');
+            return false;
+          }
+
+
+
+      let data = {
+        name:        this.name,
+        address:     this.address
+      };
+
+      console.log(data);
+
+       this.sucursalDeliverieService.RegisterSucursalDeliverie(data).subscribe((resp:any) => {
+
+        console.log(resp);
+
+        if(resp.message == 403){
+          this.toast.error('validacion', resp.message_text);
+        }else{
+          this.toast.success('Exito', 'Direccion de entrega creada correctamente');
+          this.SucursalDeliverieC.emit(resp);
+          this.modal.close();
+        }
+      });
+    }
 
 }
--- src/app/modules/configuration/sucursal-deliveries/edit-surcursal-deliveries/edit-surcursal-deliveries.component.ts
+++ src/app/modules/configuration/sucursal-deliveries/edit-surcursal-deliveries/edit-surcursal-deliveries.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { ToastrService } from 'ngx-toastr';
+import { SucursalService } from '../../sucursales/service/sucursal.service';
 
 @Component({
   selector: 'app-edit-surcursal-deliveries',
@@ -6,5 +9,65 @@ import { Component } from '@angular/core';
   styleUrls: ['./edit-surcursal-deliveries.component.scss']
 })
 export class EditSurcursalDeliveriesComponent {
+
+      @Output() SucursalDeliverieE: EventEmitter<any> = new EventEmitter();
+      @Input() SUCURSAL_SELECTED_DELIVERIE : any = [];
+
+         isLoading:      any;
+         name:           string = '';
+         address:        string = '';
+         state:          number = 1;
+
+          constructor(
+            public modal: NgbActiveModal,
+            public sucursalService: SucursalService , // Assuming RolesService is injected here
+            public toast: ToastrService,
+          ) {
+
+          }
+
+          ngOnInit(): void {
+
+            this.name       = this.SUCURSAL_SELECTED_DELIVERIE.name;
+            this.address    = this.SUCURSAL_SELECTED_DELIVERIE.address;
+            this.state      = this.SUCURSAL_SELECTED_DELIVERIE.state;
+
+          }
+
+          store(){
+
+            if(!this.name){
+              this.toast.error('validacion', 'El nombre de la Sucursal de entrega es obligatorio');
+              return false;
+            }
+
+               if(!this.address){
+              this.toast.error('validacion', 'La direccion de entrega es obligatorio');
+              return false;
+            }
+
+
+
+        let data = {
+          name:        this.name,
+          address:     this.address,
+          state:       this.state,
+        };
+
+        console.log(data);
+
+         this.sucursalService.updateSucursal(this.SUCURSAL_SELECTED_DELIVERIE.id,data).subscribe((resp:any) => {
+
+          console.log(resp);
+
+          if(resp.message == 403){
+            this.toast.error('validacion', resp.message_text);
+          }else{
+            this.toast.success('Exito', 'Sucursal de entrega editada correctamente');
+            this.SucursalDeliverieE.emit(resp);
+            this.modal.close();
+          }
+        });
+      }
 
 }

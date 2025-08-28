b2c6091955ad1ad26052d7f117ced12a26bd3aec
Sergio Morales <frezee13@hotmail.com>
2025-08-28 16:10:50 -0600

    cambios en method payments

--- src/app/modules/configuration/method-payment/delete-method-payment/delete-method-payment.component.ts
+++ src/app/modules/configuration/method-payment/delete-method-payment/delete-method-payment.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { ToastrService } from 'ngx-toastr';
+import { SucursalService } from '../../sucursales/service/sucursal.service';
+import { MethodPaymentService } from '../service/method-payment.service';
 
 @Component({
   selector: 'app-delete-method-payment',
@@ -6,5 +10,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./delete-method-payment.component.scss']
 })
 export class DeleteMethodPaymentComponent {
+
+  //emisores que vienen del componente padre en este caso ListRolesComponent
+       @Output() PaymentD: EventEmitter<any> = new EventEmitter();
+       @Input()  PAYMENTS_SELECTED: any; //EMITIR UN VALOR A ESTE COMPONENTE DESDE EL PADRE
+
+        name:          string = '';
+        isLoading:     any;
+
+
+        constructor(
+          public modal: NgbActiveModal,
+          public methodPaymentService: MethodPaymentService , // Assuming RolesService is injected here
+          public toast: ToastrService,
+        ) {
+
+        }
+
+        NgOnInit(): void {
+        }
+
+         delete()
+        {
+           this.methodPaymentService.deleteMethodPayment(this.PAYMENTS_SELECTED.id).subscribe((resp:any) => {
+             console.log(resp);
+            if(resp.message == 403){
+              this.toast.error('validacion', resp.message_text);
+            }else{
+              this.toast.success('Exito', 'Metodo de pago eliminado correctamente');
+              this.PaymentD.emit(resp);
+              this.modal.close();
+            }
+
+          });
+        }
+
 
 }
--- src/app/modules/configuration/method-payment/edit-method-payment/edit-method-payment.component.ts
+++ src/app/modules/configuration/method-payment/edit-method-payment/edit-method-payment.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { ToastrService } from 'ngx-toastr';
+import { SucursalService } from '../../sucursales/service/sucursal.service';
+import { MethodPaymentService } from '../service/method-payment.service';
 
 @Component({
   selector: 'app-edit-method-payment',
@@ -6,5 +10,65 @@ import { Component } from '@angular/core';
   styleUrls: ['./edit-method-payment.component.scss']
 })
 export class EditMethodPaymentComponent {
+
+      @Output() paymentE: EventEmitter<any> = new EventEmitter();
+      @Input() PAYMENTS_SELECTED : any = [];
+
+         isLoading:      any;
+         name:           string = '';
+         address:        string = '';
+         state:          number = 1;
+
+          constructor(
+            public modal: NgbActiveModal,
+            public methodPaymentService: MethodPaymentService , // Assuming RolesService is injected here
+            public toast: ToastrService,
+          ) {
+
+          }
+
+          ngOnInit(): void {
+
+            this.name       = this.PAYMENTS_SELECTED.name;
+            this.address    = this.PAYMENTS_SELECTED.method_payment_id;
+            this.state      = this.PAYMENTS_SELECTED.state;
+
+          }
+
+          store(){
+
+            if(!this.name){
+              this.toast.error('validacion', 'El nombre del Metodo de Pago es obligatorio');
+              return false;
+            }
+
+               if(!this.address){
+              this.toast.error('validacion', 'La Direccion del Metodo de Pago es obligatorio');
+              return false;
+            }
+
+
+
+        let data = {
+          name:        this.name,
+          address:     this.address,
+          state:       this.state,
+        };
+
+        console.log(data);
+
+         this.methodPaymentService.updateMethodPayment(this.PAYMENTS_SELECTED.id,data).subscribe((resp:any) => {
+
+          console.log(resp);
+
+          if(resp.message == 403){
+            this.toast.error('validacion', resp.message_text);
+          }else{
+            this.toast.success('Exito', 'Metodo de pago editado correctamente');
+            this.paymentE.emit(resp);
+            this.modal.close();
+          }
+        });
+      }
 
 }
